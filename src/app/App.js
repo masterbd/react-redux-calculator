bc97f606e27d8f7b52c7090b7bace3dea8869400
Dragos Bostan <dragos.bd16@gmail.com>
2019-06-03 01:48:23 +0300

    App Component code clean

--- src/app/App.js
+++ src/app/App.js
@@ -7,75 +7,6 @@ import Display from '../components/display/Display';
  * App Class Component.
  */ 
 export default class App extends React.Component {
-  // state = {
-  //   currentNumber: null,
-  //   previousNumber: null,
-  //   operation: null
-  // }
-
-  // onChangeEmitor = (data) => {
-  // if (data.type === 'operations') {
-  //   this.setState({
-  //     operation: data.value
-  //   });
-
-  // } else if (data.type === 'number') {
-  //   if (!this.state.operation) {
-  //     this.setState({
-  //       currentNumber: data.value
-  //     });
-  //   } else if (!this.state.previousNumber) {
-  //     this.setState({
-  //       previousNumber: this.state.currentNumber
-  //     });
-  //     this.setState({
-  //       currentNumber: data.value
-  //     });
-  //   } else {
-  //     this.setState({
-  //       currentNumber: this.calculate(this.state, data.value)
-  //     });
-  //     this.setState({
-  //       previousNumber: null
-  //     });
-  //     this.setState({
-  //       operation: null
-  //     });
-  //   }
-  // }
-
-  // if (data.type === 'operations') {
-  //   if (this.state.currentNumber) {
-  //     this.setState({ previousNumber: this.state.currentNumber });
-  //     console.warn('CALC', this.state.previousNumber, data.value, this.state.currentNumber);
-  //     this.setState({ currentNumber: this.calculate(this.state, data.value) });
-  //     this.setState({ previousNumber: null });
-  //   } else {
-  //     this.setState({ currentNumber: this.calculate(this.state, data.value) });
-  //   }
-  // } else if (data.type === 'number') {
-  //   this.setState({ currentNumber: data.value });
-  //   this.setState({ previousNumber: null });
-  // }
-
-  // console.warn(data, this.state);
-  // }
-
-  /**
-   * Calculate the result of current number and previous number.
-   */
-  // calculate = (numbers, operation) => {
-  //   if (operation === '-')
-  //     return numbers.previousNumber - numbers.currentNumber;
-  //   else if (operation === '+')
-  //     return numbers.previousNumber + numbers.currentNumber;
-  //   else if (operation === '*')
-  //     return numbers.previousNumber * numbers.currentNumber;
-  //   else if (operation === '/')
-  //     return numbers.previousNumber / numbers.currentNumber;
-  //   else if (operation === '=')
-  //     return numbers.currentNumber;
-  // }
 
   render() {
     return (
@@ -86,11 +17,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// const mapStateToProps = state => ({
-//   firstNumber: state.firstNumber,
-//   secondNumber: state.secondNumber,
-//   operation: state.operation
-// });
-
-// export default connect(mapStateToProps)(App);
